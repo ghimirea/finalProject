952498b1f10c7d558302962c65d94e6b87b7ea9c
Ashim Ghimire <aghimire85@gmail.com>
2020-07-24 17:07:18 -0500

    Customer can add to cart Issue #70

---
+++ mobileclient/components/Action/cart.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { ADD_CART, CART_FAIL } from '../Action/types';
+
+//! Add to Cart
+export const add_to_cart = (
+  farmer_id,
+  prod_id,
+  product_name,
+  quantity,
+  price_per_lb
+) => async (dispatch) => {
+  try {
+    const header = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const body = JSON.stringify({
+      farmer_id,
+      prod_id,
+      product_name,
+      quantity,
+      price_per_lb,
+    });
+    console.log('PRODUCTS IN CART ACTION----->', body);
+    const response = await axios.post('/cart', body, header);
+    console.log('ADD TO CART ACTION---->', response.data.msg);
+
+    dispatch({
+      type: ADD_CART,
+      payload: response.data.msg,
+    });
+  } catch (error) {
+    dispatch({
+      type: CART_FAIL,
+      //   payload: { msg: error.response, status: error.response.status },
+    });
+  }
+};
--- mobileclient/components/Reducer/reducer.js
+++ mobileclient/components/Reducer/reducer.js
@@ -3,12 +3,14 @@ import alert from './alert';
 import auth from './auth';
 import user from './user';
 import product from './product';
+import cart from './cart';
 
 const rootReducer = combineReducers({
   alert,
   auth,
   user,
   product,
+  cart,
 });
 
 export default rootReducer;
